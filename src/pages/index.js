9dc746d23b68a682299d1471b059dce5b81cf3c4
AnelejB <banjac.jelena8@gmail.com>
2021-01-16 11:53:10 +0100

    Added google verification second try

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,18 @@ import Layout from "../components/layout"
 import Footer from '../components/footer'
 import SEO from "../components/seo"
 import './index.css'
+import Helmet from 'react-helmet'
+
+
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <html lang="en" />
+      <title>Tamarina Masta Moze Svasta</title>
+      <meta name="google-site-verification" content="qnT69VP4ryMoVLO06OnWgYtDKtfezfChQEsfX8KNY-A" />
+    </Helmet>
+
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <Nav />
     <Featured />
